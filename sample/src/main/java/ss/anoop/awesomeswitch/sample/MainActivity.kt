b0ab2c7cd8e46406613551c0d653e0edc885c907
Anoop S S <anoopvvs@gmail.com>
2019-10-13 21:57:45 +0530

    Awesome Switch sample

---
+++ sample/src/main/java/ss/anoop/awesomeswitch/sample/MainActivity.kt
@@ -0,0 +1,16 @@
+package ss.anoop.awesomeswitch.sample
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.layout_main_activity.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.layout_main_activity)
+
+    }
+}
